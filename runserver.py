c011558759affdc27f161b97e8567d16d3fe83ad
Tim Hopper <tdhopper@gmail.com>
2015-07-19 12:01:28 -0400

    Change port to localhost

--- runserver.py
+++ runserver.py
@@ -14,4 +14,4 @@ elif __name__ == '__main__':
     from flask_application import app
     app.debug = True
     app.config['DEBUG'] = True
-    app.run(host = "0.0.0.0", port = 8080)
+    app.run(host = "127.0.0.1", port = 8080)
