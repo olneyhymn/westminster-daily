c8c3577fe1d2928acb9a1e814f1506e67e5c0f0e
Tim Hopper <tdhopper@gmail.com>
2015-07-25 17:17:44 -0400

    Remove unused task/add logger

--- fabfile.py
+++ fabfile.py
@@ -28,129 +28,15 @@ APP_NAME = "Westminster Daily"
 PROJ_DIR = os.path.dirname(os.path.abspath(__file__))
 SITE_NAME = "reformedconfessions.com"
 
-env.hosts = ['user@remotehost']
-code_dir = '/path/to/repos/destination'
 sys.path.append(PROJ_DIR)
 
-"""
-def _transfer_files(src, dst, ssh_port=None):
-    ssh_port = ssh_port or 22
-    assert os.getenv('SSH_AUTH_SOCK') is not None # Ensure ssh-agent is running
-    if not src.endswith('/'):
-        src = src + '/'
-    if dst.endswith('/'):
-        dst = dst[:-1]
-    local('rsync -avh --delete-before --copy-unsafe-links -e "ssh -p {0}" {1} {2}'.format(ssh_port, src, dst), capture=False)
-"""
-
-
-@task
-def pre_deploy():
-    '''Add, commit and push the Git repo before final deployment.'''
-    local("git add -p && git commit")
-    local("git push")
-
-
-@task
-def deploy():
-    global code_dir
-    '''Final deployment of the application to Heroku'''
-    with cd(code_dir):
-        run("git pull")
-        run("git push heroku master")
-
-
-@task
-def init(site_name=SITE_NAME):
-    '''Call env_setup, env_init, and skeletonize for one-step init'''
-    print green(u"Call env_setup, env_init, and skeletonize for one-step init:")
-    env_setup()
-    env_init(site_name=site_name)
-    skeletonize()
-
-
-@task
-def env_init(site_name=SITE_NAME):
-    '''Initialize with this site hostname.'''
-    print green(u"Initializing new site configuration...")
-
-    #
-    # Generate secret key and update config file
-    #
-    import random
-    import string
-
-    CHARS = string.letters + string.digits
-    SECRET_KEY = "".join([random.choice(CHARS) for i in range(50)])
-
-    print blue("Configuring the secret key...")
-    os.chdir(PROJ_DIR)
-    try:
-        sh.sed("-i.bak",
-               "-e s/SECRET_KEY *= *.*/SECRET_KEY = '{0}'/g".format(SECRET_KEY),
-               "{0}/config.py".format(APP_NAME))
-        sh.rm(f="config.py.bak")
-    except sh.ErrorReturnCode:
-        print red("Could not configure SECRET_KEY for config.py")
-        exit(1)
-
-    #
-    # Set the site name, the user defined site hostname
-    #
-    print blue("Configuring the SITE_NAME '{0}'.".format(site_name))
-    try:
-        sh.sed("-i.bak",
-               "-es/SITE_NAME\s*=\s*.*/SITE_NAME = '{0}'/g".format(site_name),
-               "{0}/config.py".format(APP_NAME))
-        sh.rm(f="config.py.bak")
-    except sh.ErrorReturnCode:
-        print red("Could not configure SITE_NAME for config.py")
-        exit(1)
-
-
-@task
-def env_setup():
-    '''Initialize environment with requisite Python modules.'''
-    print green("Installing requisite modules...")
-
-    # Install our requistite modules for the website.
-    sh.pip("install", r="requirements.txt")
-
-    import platform
-    if platform.python_version_tuple() < (2, 7):
-        sh.pip("install", "unittest2")
-
-
-@task
-def skeletonize():
-    '''Update Skeleton HTML5-Boilerplate.'''
-    print green("Skeletonizing the project directory...")
-
-    # Skeleton
-    print blue("Installing skeleton HTML5 Boilerplate.")
-    os.chdir(PROJ_DIR)
-    sh.git.submodule.update(init=True)
-
-    os.chdir(PROJ_DIR + "/skeleton")
-    # sh.git.pull("origin", "master")
-    sh.rsync("-av", "images", "{0}/{1}/static/".format(PROJ_DIR, APP_NAME))
-    sh.rsync("-av", "stylesheets", "{0}/{1}/static/".format(PROJ_DIR, APP_NAME))
-    sh.rsync("-av", "index.html", "{0}/{1}/templates/base_t.html".format(PROJ_DIR, APP_NAME))
-    os.chdir(PROJ_DIR)
-
-    # Patch the base template with templating tags
-    print blue("Patching the base template.")
-    os.chdir(PROJ_DIR + "/{0}/templates/".format(APP_NAME))
-    template_patch = open("base_t.patch".format(APP_NAME))
-    sh.patch(strip=0, _in=template_patch)
-    template_patch.close()
-    os.chdir(PROJ_DIR)
-
-    # Jquery
-    print blue("Installing jquery 1.9.0.")
-    os.chdir(PROJ_DIR + "/" + APP_NAME + "/static/js")
-    sh.curl("http://code.jquery.com/jquery-1.9.0.min.js", O=True)
-    os.chdir(PROJ_DIR)
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+log.addHandler(ch)
 
 
 @task
