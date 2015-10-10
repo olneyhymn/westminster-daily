b6c95720fa8852a9935cc1580bbec3ba315847fd
Tim Hopper <tdhopper@gmail.com>
2015-10-10 11:47:20 -0400

    Validate data returns things

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import pytz
+import pytest
 
 import flask_application.data as data
 
@@ -38,7 +39,7 @@ def test_confession():
                             "will unto his people being now ceased."
 
 
-def test_larger_catechism():
+def test_larger_catechism_q1():
     wlc11 = data.get_catechism('wlc', '1')
     assert wlc11['type'] == 'catechism'
     assert wlc11['abbv'] == 'wlc'
@@ -49,3 +50,44 @@ def test_larger_catechism():
     assert wlc11['long_citation'] == 'Larger Catechism 1'
     assert wlc11['answer'] == 'Man\'s chief and highest end is to glorify God, and fully to enjoy him forever.'
     assert wlc11['question'] == 'What is the chief and highest end of man?'
+
+
+def test_larger_catechism_all_exists():
+    for question in range(1, 197):
+        assert data.get_catechism('wlc', str(question))
+    with pytest.raises(data.DataException):
+        data.get_catechism('wlc', "198")
+    with pytest.raises(data.DataException):
+        data.get_catechism('wlc', "0")
+
+
+
+def test_shorter_catechism_all_exists():
+    for question in range(1, 107):
+        assert data.get_catechism('wsc', str(question))
+    with pytest.raises(data.DataException):
+        data.get_catechism('wsc', "108")
+    with pytest.raises(data.DataException):
+        data.get_catechism('wsc', "0")
+
+
+def test_westminster_confession_all_exists():
+    for c, s in [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7), (1, 8), (1, 9), (1, 10),
+                 (2, 1), (2, 2), (2, 3), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (3, 7),
+                 (3, 8), (4, 1), (4, 2), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (5, 7),
+                 (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6), (7, 1), (7, 2), (7, 3), (7, 4),
+                 (7, 5), (7, 6), (8, 1), (8, 2), (8, 3), (8, 4), (8, 5), (8, 6), (8, 7), (8, 8),
+                 (9, 1), (9, 2), (9, 3), (9, 4), (9, 5), (10, 1), (10, 2), (10, 3), (10, 4), (11, 1),
+                 (11, 2), (11, 3), (11, 4), (11, 5), (11, 6), (12, 1), (13, 1), (13, 2), (13, 3), (14, 1),
+                 (14, 2), (14, 3), (15, 1), (15, 2), (15, 3), (15, 4), (15, 5), (15, 6), (16, 1), (16, 2),
+                 (16, 3), (16, 4), (16, 5), (16, 6), (16, 7), (17, 1), (17, 2), (17, 3), (18, 1), (18, 2),
+                 (18, 3), (18, 4), (19, 1), (19, 2), (19, 3), (19, 4), (19, 5), (19, 6), (19, 7), (20, 1),
+                 (20, 2), (20, 3), (20, 4), (21, 1), (21, 2), (21, 3), (21, 4), (21, 5), (21, 6), (21, 7),
+                 (21, 8), (22, 1), (22, 2), (22, 3), (22, 4), (22, 5), (22, 6), (22, 7), (23, 1), (23, 2),
+                 (23, 3), (23, 4), (24, 1), (24, 2), (24, 3), (24, 4), (24, 5), (24, 6), (25, 1), (25, 2),
+                 (25, 3), (25, 4), (25, 5), (25, 6), (26, 1), (26, 2), (26, 3), (27, 1), (27, 2), (27, 3),
+                 (27, 4), (27, 5), (28, 1), (28, 2), (28, 3), (28, 4), (28, 5), (28, 6), (28, 7), (29, 1),
+                 (29, 2), (29, 3), (29, 4), (29, 5), (29, 6), (29, 7), (29, 8), (30, 1), (30, 2), (30, 3),
+                 (30, 4), (31, 1), (31, 2), (31, 3), (31, 4), (32, 1), (32, 2), (32, 3), (33, 1), (33, 2),
+                 (33, 3)]:
+        assert data.get_confession("wcf", str(c), s)
