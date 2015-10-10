88d606af06cb0ab45266ec0939cb70abbc82e681
Tim Hopper <tdhopper@gmail.com>
2015-10-10 12:53:30 -0400

    Restructure for clarity

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,6 +4,18 @@ import pytest
 
 import flask_application.data as data
 
+WCF11 = "Although the light of nature, and the works " \
+        "of creation and providence do so far manifest the goodness, wisdom, and power of " \
+        "God, as to leave men unexcusable; yet are they not sufficient to give that " \
+        "knowledge of God, and of his will, which is necessary unto salvation. Therefore " \
+        "it pleased the Lord, at sundry times, and in divers manners, to reveal himself, " \
+        "and to declare that his will unto his church; and afterwards, for the better " \
+        "preserving and propagating of the truth, and for the more sure establishment and " \
+        "comfort of the church against the corruption of the flesh, and the malice of " \
+        "Satan and of the world, to commit the same wholly unto writing: which maketh the " \
+        "Holy Scripture to be most necessary; those former ways of God's revealing his " \
+        "will unto his people being now ceased."
+
 
 def test_today_content():
     month, day, content = data.get_today_content(tz='UTC')
@@ -42,17 +54,7 @@ def test_confession():
     assert wcf11['paragraph'] == "1"
     assert wcf11['citation'] == "WCF 1.1"
     assert wcf11['long_citation'] == "Confession of Faith 1.1"
-    assert wcf11['body'] == "Although the light of nature, and the works " \
-                            "of creation and providence do so far manifest the goodness, wisdom, and power of " \
-                            "God, as to leave men unexcusable; yet are they not sufficient to give that " \
-                            "knowledge of God, and of his will, which is necessary unto salvation. Therefore " \
-                            "it pleased the Lord, at sundry times, and in divers manners, to reveal himself, " \
-                            "and to declare that his will unto his church; and afterwards, for the better " \
-                            "preserving and propagating of the truth, and for the more sure establishment and " \
-                            "comfort of the church against the corruption of the flesh, and the malice of " \
-                            "Satan and of the world, to commit the same wholly unto writing: which maketh the " \
-                            "Holy Scripture to be most necessary; those former ways of God's revealing his " \
-                            "will unto his people being now ceased."
+    assert wcf11['body'] == WCF11
 
 
 def test_larger_catechism_q1():
