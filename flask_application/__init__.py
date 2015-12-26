e0d113646baefc8c8537b5ddf5034e930c6717f7
Tim Hopper <tdhopper@gmail.com>
2015-12-26 14:02:02 -0500

    Add About page

--- flask_application/__init__.py
+++ flask_application/__init__.py
@@ -6,6 +6,7 @@ from functools import wraps
 from flask import Flask, render_template
 from flask import Markup, request, redirect
 from flask import send_from_directory
+from markdown import markdown
 from werkzeug.routing import BaseConverter
 from werkzeug.contrib.atom import AtomFeed
 from werkzeug.contrib.cache import SimpleCache
@@ -132,6 +133,42 @@ def reading_plan():
     dates = [start + dt.timedelta(days=i) for i in range(365)]
     content = [(d, data.get_day_title(d.month, d.day)) for d in dates]
     return render_template('reading_plan_t.html',
+                           page_title=page_title,
+                           content=content)
+
+
+@app.route('/about')
+@cached()
+def about_page():
+    page_title = "A Daily Reading"
+    content = Markup(markdown("""
+Since the 17th century, the Westminster Confession of Faith and Catechisms have served as a doctrinal standard for Presbyterian churches. In the Orthodox Presbyterian Church, "Ministers, elders, and deacons... are required to believe the Bible as the only infallible rule of faith and practice, to sincerely receive and adopt the Confession of Faith and Catechisms as containing the system of doctrine taught in Scripture" (Preface to the Book of Church Order, 2014).
+
+The Westminster Confession organizes the core teaching of Scripture in thirty three, topically-arranged chapters. The Shorter Catechism outlines the theology of Scripture in the form of 107 questions and answers. The Larger Catechism expands on the Shorter with 196 questions and (typically longer) answers.
+
+Dr. Chad Van Dixhorn's commendation [the Larger Catechism](http://www.opc.org/new_horizons/NH00/0010b.html) points the reader to benefit of studying all three documents:
+
+> There are many reasons why the Larger Catechism is worth our study. It unifies Presbyterians who use it as one of their church standards. It gives us the meat of the Word of God. It places a greater emphasis on, and gives fuller explanations of, doctrines that maturing Christians need to hear. It emphasizes aspects of the gospel and draws directly from Scripture in a way that other catechisms do not. And the Larger Catechism emphasizes the church, the ministry, preaching, and the sacraments at a time when Presbyterians&mdash;and in fact all Christians&mdash;need to hear of them.
+
+To encourage Christians to read the Westminster Standards, Dr. Joey Pipa Jr. has prepared [a calendar of readings](/westminster-daily/reading-plan) from these three documents. By following his calendar, you will read through the Standards every year. With this site, you can find the daily readings at [reformedconfessions.com/westminster-daily](http://www.reformedconfessions.com/westminster-daily), on [Facebook](https://www.facebook.com/westminsterdaily/), on [Twitter](https://twitter.com/refconfessions), and in your [email](https://feed.press/e/mailverify?feed_id=westminster-daily).
+
+Dr. Pipa provides the follow recommendations with [his original
+plan](https://www.gpts.edu/resources/documents/Calendar%20Readings%20in%20WestminsterNumbered.pdf).
+
+1. Use this calendar along with your daily Bible reading or with your
+family in family worship.
+2. Read section for the day and read proof texts.
+3. Memorize the Shorter Catechism on days assigned. Review the
+Shorter Catechism on the Lord's Day.
+4. Select Scripture texts to memorize along with catechism and
+review on the Lord's Day as well.
+Some catechism questions are out of numerical order for thematic
+purposes. The format coordinates with that used in the "Harmony of the
+Westminster Confession and Catechism" by Dr. Morton Smith.
+
+May the Lord bless you as you "press on" to know Him ([Hosea 6](http://www.esvbible.org/Hosea6:3/)).
+"""))
+    return render_template('paragraph_page_t.html',
                            page_title=page_title,
                            content=content)
 
