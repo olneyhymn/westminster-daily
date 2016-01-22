dbb82ee86aa919881cf0f5f9a809f1e7f4b1e467
Tim Hopper <tdhopper@gmail.com>
2016-01-22 12:00:14 -0500

    Clean up runserver.py

--- runserver.py
+++ runserver.py
@@ -2,16 +2,15 @@
 import sys
 from pprint import pprint
 
+
 def show(obj):
     '''Show the dump of the properties of the object.'''
     pprint(vars(obj))
 
+
 if sys.flags.interactive:
     from flask_application import *
-    #from flask_application.models import *
     print 'Loading Flask App in console mode. Use show(<obj)> to introspect.'
 elif __name__ == '__main__':
     from flask_application import app
-    app.debug = True
-    app.config['DEBUG'] = True
     app.run(host = "127.0.0.1", port = 8080)
