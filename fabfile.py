ed84f6e26c978342c6761d74209254c19e54a997
Tim Hopper <tdhopper@gmail.com>
2016-01-23 09:23:21 -0500

    Add confesstion to yaml command

--- fabfile.py
+++ fabfile.py
@@ -243,3 +243,38 @@ def catechism_to_yaml(name="wlc"):
                .replace("2answer", "A")
                .replace("3prooftexts", "proofs")
            )
+
+
+@task
+def confession_to_yaml():
+    """Generate YAML file for WCF"""
+    import re
+    import json
+    import yaml
+
+    with open('../westminster-daily/flask_application/static/confessions/wcf.json', 'r') as f:
+        catechism = json.load(f, object_hook=ascii_encode_dict)
+
+    for k, v in catechism.items():
+        del v['prooftext_verses']
+
+    catechism2 = {}
+    for k, v in catechism.items():
+        catechism2[int(k)] = v
+        for pk in list(v['prooftexts'].keys()):
+            v['prooftexts'][int(pk)] = v['prooftexts'][pk]
+            del v['prooftexts'][(pk)]
+        body = {int(k): v for k, v in v['body'].items()}
+        for k2, v2 in body.items():
+            pattern = r"<span data-prooftexts='.*?' data-prooftexts-index='([0-9]+)'>(.*?)</span>"
+
+            body[k2] = re.sub(pattern, r"\2[\1]", v2)
+
+        #
+        v['body'] = body
+        v['1title'] = v['title']
+        del v['title']
+
+    print (yaml.dump(catechism2, default_flow_style=False, width=120)
+               .replace("1title", "title")
+           )
