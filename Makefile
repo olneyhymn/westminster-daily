bc262c926872082bd177c4e2d2384824e65e5da0
Tim Hopper <tdhopper@gmail.com>
2017-08-29 22:04:34 -0400

    add stuff to update facebook and twitter from lambda

--- Makefile
+++ Makefile
@@ -2,6 +2,22 @@
 clean:
 	rm -rf flask_application/build
 
+
+dependencies:
+
+
+lambda:
+	mkdir -p lambda
+	cp -r flask_application lambda/
+	rm -rf lambda/flask_application/static/images
+	rm -rf lambda/flask_application/build
+	cp update.py lambda/
+	cp requirements.txt lambda/
+	cd lambda && STATIC_DEPS=true pip install -U retrying facebook-sdk twitter pytz -t .
+	zip -r lambda_bundle lambda/*
+	# make clean
+
+
 build: flask_application/build
 
 flask_application/build:
@@ -14,4 +30,4 @@ s3_upload: build
 	s3cmd sync --acl-public --delete-removed flask_application/build/ s3://reformedconfessions.com/
 
 
-.PHONY: s3_upload clean build
+.PHONY: s3_upload clean build lambda
--- update.py
+++ update.py
@@ -1,10 +1,16 @@
 import boto3
 import datetime as dt
-
+import os
+import facebook
+import pytz
+import requests
+from retrying import retry
+from urllib.request import urlopen
 
 bucket = 'reformedconfessions.com'
 index = 'westminster-daily/index.html'
 feed = 'westminster-daily/feed.rss'
+base_url = 'http://reformedconfessions.com.s3-website-us-east-1.amazonaws.com/westminster-daily/'
 
 
 def update_feed_and_homepage(a, b):
@@ -29,3 +35,99 @@ def update_feed_and_homepage(a, b):
             )
     s3.put_object_acl(ACL='public-read', Bucket=bucket, Key=feed)
     s3.put_object_acl(ACL='public-read', Bucket=bucket, Key=index)
+
+
+def update_facebook(a, b):
+    api = facebook.GraphAPI(os.environ['FB_ACCESS_TOKEN'])
+
+    month, day, content = get_today_content(tz="US/Eastern", prooftexts=False)
+    url = "{base}{month:0>2}/{day:0>2}".format(base=base_url, month=month, day=day)
+    attachment = {'link': url}
+
+    content = make_facebook_string(content)
+
+    if (month, day) in [(1, 4), (7, 5)]:
+        content = "" # Days have html formatting
+
+    _post_facebook(api, content, attachment)
+
+
+@retry(wait_exponential_multiplier=1000,
+       stop_max_attempt_number=10,
+       retry_on_exception=facebook.GraphAPIError)
+def _post_facebook(api, content, attachment):
+    api.put_wall_post(content, attachment=attachment)
+
+
+def make_facebook_string(content):
+    c_strings = []
+    for c in content:
+        if c['type'] == 'confession':
+            c_strings.append("""{}
+
+{}
+
+""".format(c['long_citation'], c['body']))
+        elif c['type'] == 'catechism':
+            c_strings.append("""{}
+
+Q. {}
+A. {}
+
+""".format(c['long_citation'], c['question'], c['answer']))
+    return ''.join(c_strings)
+
+
+def tweet(a, b):
+    """Send tweet with todays content"""
+    import twitter as tw
+    base_url = "http://reformedconfessions.com/westminster-daily/"
+    cred = {
+        "consumer_key": os.environ['TW_CONSUMER_KEY'],
+        "consumer_secret": os.environ['TW_CONSUMER_SECRET'],
+        "token": os.environ['TW_TOKEN'],
+        "token_secret": os.environ['TW_TOKEN_SECRET'],
+    }
+    auth = tw.OAuth(**cred)
+    t = tw.Twitter(auth=auth)
+
+    month, day, content = get_today_content(tz="US/Eastern")
+    description = get_day_title(month, day)
+    url = "{base}{month:0>2}/{day:0>2}".format(base=base_url, month=month, day=day)
+
+    try:
+        # Attempt tweet
+        url = "http://www.reformedconfessions.com/static/images/docs/{:0>2}{:0>2}-full.png".format(month, day)
+        imagedata = urlopen(url).read()
+        t_up = tw.Twitter(domain='upload.twitter.com', auth=auth)
+        id_img1 = t_up.media.upload(media=imagedata)["media_id_string"]
+        t.statuses.update(status="{} {}".format(description, url),
+                          media_ids=id_img1)
+    except tw.api.TwitterHTTPError as e:
+        if any(error['code'] == 186 for error in e.response_data['errors']):
+            # Tweet too long. Try a shorter tweet.
+            description = ", ".join(c['citation'] for c in content)
+            t.statuses.update(status="{} {}".format(description, url))
+        else:
+            pass
+            # log.error("%s %s", "Unhandled exception", str(e))
+        return
+    # log.info("%s %s", "Tweeted", str(description))
+
+
+def get_today_content(tz="US/Eastern", prooftexts=False):
+    tz = pytz.timezone(tz)
+    month = dt.datetime.now(tz=tz).strftime('%m')
+    day = dt.datetime.now(tz=tz).strftime('%d')
+    return month, day, get_day(month, day, prooftexts)['content']
+
+
+def get_day(month, day, prooftexts=False):
+
+    url = "{base_url}{month}/{day}.json".format(base_url=base_url, month=month, day=day)
+    print(url)
+    return requests.get(url).json()
+
+
+def get_day_title(month, day):
+    return get_day(month, day)['title']
